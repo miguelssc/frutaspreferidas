ba090f14dfbdd6b04e6bee601b4d40fb100359e4
miguel <miguelpositivo02@gmail.com>
2023-03-03 20:41:14 -0300

    finalizar exercicio frutas preferidas

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("--- Minhas Frutas Preferidas ---");
+Console.WriteLine();
+Console.WriteLine("1-Manga");
+Console.WriteLine("2-Morango");
+Console.WriteLine("3-Abacaxi");
+Console.WriteLine("4-Melancia");
+Console.WriteLine("5-Banana");
